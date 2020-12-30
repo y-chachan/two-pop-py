057eb216d59de6436fa2a338b8f3c1f8c475fac0
y-chachan <yayaatichachan@gmail.com>
2020-12-30 18:28:26 +0530

    add M_earth to constants

--- twopoppy/const.py
+++ twopoppy/const.py
@@ -3,6 +3,7 @@ set some general constants in CGI units
 """
 import numpy as np
 import scipy.constants as sc
+import astropy.constants as astropyc
 
 pi           = np.pi;                   # noqa - PI
 k_b          = sc.k*1e7;                # noqa - Boltzmann constant in erg/K
@@ -14,3 +15,4 @@ mu           = 2.3e0;                   # noqa - mean molecular mass in proton m
 M_sun        = 1.9891e+33;              # noqa - mass of the sun in g
 R_sun        = 69550800000.0;           # noqa - radius of the sun in cm
 sig_h2       = 2e-15;                   # noqa - cross section of H2 [cm^2]
+M_earth      = astropyc.M_earth.cgs.value
